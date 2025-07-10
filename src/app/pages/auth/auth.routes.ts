420b6ad447f6097dbb52c232b8572716e8dbdb35
Carlos Henrique <carlosmvsep3@hotmail.com>
2025-07-09 22:40:47 -0300

    #Excluido access e error do acesso

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,13 +1,4 @@
 import { Routes } from '@angular/router';
-import { Access } from './access';
-import { Login } from './login';
-import { Error } from './error';
-
-// export const authRoutes: Routes [
-//     { path: 'access', loadComponent: Access },
-//     { path: 'error', component: Error },
-//     { path: 'login', component: Login }
-// ];
 
 const login = () => import('./login').then(x => x.Login);
 
@@ -17,3 +8,4 @@ export const loginRoute: Routes = [
         loadComponent: login
     }
 ];
+
